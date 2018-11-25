8d87f62e6c0b27f439c08be8012d1b1d3ebb984c
as <as.utf8@gmail.com>
2018-11-25 14:42:01 -0800

    fix and add check for off-by-1

--- 28938/main_test.go
+++ 28938/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 	"testing"
 	"time"
 )
@@ -9,11 +10,11 @@ import (
 const (
 	E         = 20
 	Size      = 1 << E
-	maxWriter = 2
+	maxWriter = 1 // 2 will exceed 10 minutes on my system
 )
 
 func hammer(c Cache, done chan bool) {
-	r := newXor(Size)
+	r := newXor(Size - 1)
 	for {
 		select {
 		case <-done:
@@ -28,7 +29,7 @@ func hammer(c Cache, done chan bool) {
 	}
 }
 func getter(c Cache, done chan bool) {
-	r := newXor(Size)
+	r := newXor(Size - 1)
 	for {
 		select {
 		case <-done:
@@ -37,6 +38,22 @@ func getter(c Cache, done chan bool) {
 		}
 		c.Get(r.next())
 	}
+}
+
+func TestRandtab(t *testing.T) {
+	t.Skip("usability test skipped")
+	var hit = [Size]int{}
+	n := 0
+	for _, v := range Randtab {
+		hit[v]++
+		n++
+	}
+	ssq := 0.0
+	for _, v := range hit {
+		p := float64(v) / float64(n)
+		ssq += p * p
+	}
+	t.Log(ssq) //
 }
 
 func TestMap(t *testing.T) {
@@ -72,7 +89,7 @@ func BenchmarkMap(b *testing.B) {
 		} {
 			// Iterate in series and randomly
 			for _, ctrFunc := range []func(uint32) seq{
-				newCtr, newXor,
+				newCtr, newXor, // newMem,
 			} {
 				c := new(Size)
 				done := make(chan bool)
@@ -105,9 +122,40 @@ func BenchmarkMap(b *testing.B) {
 	}
 }
 
+func init() {
+	x := newXor(Size - 1)
+	for i := 0; i < len(Randtab); i++ {
+		Randtab[i] = x.next()
+	}
+}
+
+var Randtab = [Size]int{}
+
 type seq interface {
 	next() int
 }
+
+type mem struct {
+	int  uint32
+	mask uint32
+}
+
+func (c *mem) next() int {
+	c.int++
+	return Randtab[c.int&c.mask]
+}
+
+func cm(mask uint32) uint32 {
+	n := bits.OnesCount32(mask + 1)
+	if n > 1 {
+		panic(fmt.Sprintf("mask %x not p-1 congruent", mask))
+	}
+	return mask
+}
+
+func newCtr(mask uint32) seq { return &ctr{^uint32(0), cm(mask)} }
+func newMem(mask uint32) seq { return &mem{^uint32(0), cm(mask)} }
+func newXor(mask uint32) seq { return &xorshift32{uint32(time.Now().Unix()), cm(mask)} } // prime
 
 type ctr struct {
 	int  uint32
@@ -118,13 +166,10 @@ type xorshift32 struct {
 	mask uint32
 }
 
-func newCtr(mask uint32) seq { return &ctr{^uint32(0), mask} }
 func (c *ctr) next() int {
 	c.int++
 	return int(c.int & c.mask)
 }
-
-func newXor(mask uint32) seq { return &xorshift32{uint32(time.Now().UnixNano()), mask} } // prime
 func (x *xorshift32) next() int {
 	x.v ^= x.v << 13
 	x.v ^= x.v >> 17
@@ -132,5 +177,6 @@ func (x *xorshift32) next() int {
 	return int(x.v & x.mask)
 }
 
+func (m mem) String() string        { return fmt.Sprintf("Memo/Mask%x", m.mask) }
 func (x xorshift32) String() string { return fmt.Sprintf("Rand/Mask%x", x.mask) }
 func (c ctr) String() string        { return fmt.Sprintf("Incr/Mask%x", c.mask) }
